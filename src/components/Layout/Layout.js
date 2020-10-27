04111e18436de728ed6b2b9d5b0e524ee3786413
Ashleen Rath <ashleenrath@Ashleens-MacBook-Pro.local>
2020-10-27 13:52:21 +0000

    Navigation Toolbar placeholder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Aux from "../../containers/hoc/Aux";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
   <Aux>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
